b1e9415d46924649e8a55e0eb7a9a2f241652ac2
Maxime EVENNOU <maxime.evennou0406@gmail.com>
2018-05-18 11:32:40 +0200

    deuxième version

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mon Site</title>
+        <meta charset="UTF-8">
+
+        <style>
+        
+            body {
+                background: linear-gradient(silver, black);
+            }
+            p { 
+                font-size: 80px;
+                color: silver;
+                text-align: center;
+            }
+            p:hover{
+                color: gold; 
+                font-weight: bold;
+                font-size: 100px;
+            }
+            .container{
+                color: gold;
+                text-align: center;
+                background: black;
+                padding: 60px;
+            }
+            footer{
+                background: black;
+                color: gold;
+                text-align: center;
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <p>Hello !!!</p>
+
+    <div class="container">
+         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia enim provident a quis corporis, veritatis, nihil minima rem fugit nulla numquam dolores soluta veniam. Necessitatibus debitis tempore, aliquam officia ullam possimus officiis repellat magni incidunt error beatae facere nisi hic omnis dolore aspernatur inventore fugiat! Sint, ullam repudiandae eum impedit cum voluptas eos neque ab delectus voluptatibus architecto dolor cupiditate maxime fugit nulla suscipit facilis, libero tempora beatae necessitatibus sunt minus exercitationem deserunt! Voluptatum ratione debitis doloremque temporibus doloribus ipsum inventore repudiandae, quasi laudantium adipisci ducimus nesciunt nostrum! Assumenda officia quia eveniet cumque laboriosam exercitationem nam sint inventore ipsa accusantium maiores rem velit est, deleniti libero labore architecto nulla? Sunt aut quidem eaque sequi suscipit nostrum itaque nobis non accusantium enim, accusamus dignissimos officiis modi perferendis pariatur magnam porro qui asperiores architecto repellat? Pariatur magnam quibusdam officia sint, tempora vero impedit repellat inventore? Reprehenderit, nobis eum? Voluptatum laudantium harum quis, labore possimus rerum corporis vel blanditiis itaque nesciunt quasi laboriosam animi ipsa alias voluptatibus eligendi cumque eum voluptate obcaecati maxime quae. Iste obcaecati voluptate, sequi, consectetur ab ipsum voluptatem minus iure, perferendis laboriosam excepturi quaerat repudiandae alias? Perferendis omnis deleniti explicabo nam! Voluptas ex optio maiores quisquam error tempore sed!
+    </div>
+    <footer>
+        &copy; Create by EVENNOU Maxime
+    </footer>
+    </body>
+</html>
