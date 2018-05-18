b12eba0184460f71027d0465173eb5120ea23a95
Maxime EVENNOU <maxime.evennou0406@gmail.com>
2018-05-18 12:05:36 +0200

    ajout html

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
             body {
                 background: linear-gradient(silver, black);
             }
-            p { 
+            .titre p { 
                 font-size: 80px;
                 color: silver;
                 text-align: center;
             }
-            p:hover{
+            .titre p:hover{
                 color: gold; 
                 font-weight: bold;
                 font-size: 100px;
@@ -25,23 +25,46 @@
                 background: black;
                 padding: 60px;
             }
+            .city h2{
+                color: gold;
+                text-align: center;
+            }
+            .city p{
+                color: white;
+                text-align: center;
+            }
             footer{
                 background: black;
                 color: gold;
                 text-align: center;
             }
+           
 
         </style>
     </head>
 
     <body>
+    <div class="titre">
         <p>Hello !!!</p>
+    </div>
 
     <div class="container">
          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia enim provident a quis corporis, veritatis, nihil minima rem fugit nulla numquam dolores soluta veniam. Necessitatibus debitis tempore, aliquam officia ullam possimus officiis repellat magni incidunt error beatae facere nisi hic omnis dolore aspernatur inventore fugiat! Sint, ullam repudiandae eum impedit cum voluptas eos neque ab delectus voluptatibus architecto dolor cupiditate maxime fugit nulla suscipit facilis, libero tempora beatae necessitatibus sunt minus exercitationem deserunt! Voluptatum ratione debitis doloremque temporibus doloribus ipsum inventore repudiandae, quasi laudantium adipisci ducimus nesciunt nostrum! Assumenda officia quia eveniet cumque laboriosam exercitationem nam sint inventore ipsa accusantium maiores rem velit est, deleniti libero labore architecto nulla? Sunt aut quidem eaque sequi suscipit nostrum itaque nobis non accusantium enim, accusamus dignissimos officiis modi perferendis pariatur magnam porro qui asperiores architecto repellat? Pariatur magnam quibusdam officia sint, tempora vero impedit repellat inventore? Reprehenderit, nobis eum? Voluptatum laudantium harum quis, labore possimus rerum corporis vel blanditiis itaque nesciunt quasi laboriosam animi ipsa alias voluptatibus eligendi cumque eum voluptate obcaecati maxime quae. Iste obcaecati voluptate, sequi, consectetur ab ipsum voluptatem minus iure, perferendis laboriosam excepturi quaerat repudiandae alias? Perferendis omnis deleniti explicabo nam! Voluptas ex optio maiores quisquam error tempore sed!
     </div>
+    <div class="city">
+        <h2>London</h2>
+            <p>London is the capital of England.</p>
+
+        <h2>Paris</h2>
+            <p>Paris is the capital of France.</p>
+
+        <h2>Tokyo</h2>
+        <p>Tokyo is the capital of Japan.</p>
+    </div>
+
     <footer>
-        &copy; Create by EVENNOU Maxime
+        &copy; Created by J-C, Rachma & Maxime
     </footer>
+
     </body>
 </html>
